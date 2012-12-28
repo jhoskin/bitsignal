ec74f0045926c25817baf9ecc2e3797ea47b6cdc
Joh Hoskin <joh@dev.geek.nz>
2012-12-28 17:34:43 +1100

    Strip out marionette and replace with angular.js

--- bitsignal.web/Content/js/main.coffee
+++ bitsignal.web/Content/js/main.coffee
@@ -1,21 +1,6 @@
-require.config
-	paths :
-		backbone : 'backbone.marionette/backbone'
-		underscore : 'backbone.marionette/underscore'
-		jquery : 'backbone.marionette/jquery'
-		marionette : 'backbone.marionette/backbone.marionette'
-	shim :
-		jquery : 
-			exports : 'jQuery'
-		underscore :
-			exports : '_'
-		backbone :
-			deps : ['jquery', 'underscore']
-			exports : 'Backbone'
-		marionette :
-			deps : ['jquery', 'underscore', 'backbone']
-			exports : 'Marionette'
 
-require ['jquery','app'], ->
+window.RegisterController = ($scope, $http) ->
+	$scope.register = (user) ->
+		$http.post('Register', user)
 	
 	
--- bitsignal.web/Content/js/main.js
+++ bitsignal.web/Content/js/main.js
@@ -1,30 +1,9 @@
 (function() {
 
-  require.config({
-    paths: {
-      backbone: 'backbone.marionette/backbone',
-      underscore: 'backbone.marionette/underscore',
-      jquery: 'backbone.marionette/jquery',
-      marionette: 'backbone.marionette/backbone.marionette'
-    },
-    shim: {
-      jquery: {
-        exports: 'jQuery'
-      },
-      underscore: {
-        exports: '_'
-      },
-      backbone: {
-        deps: ['jquery', 'underscore'],
-        exports: 'Backbone'
-      },
-      marionette: {
-        deps: ['jquery', 'underscore', 'backbone'],
-        exports: 'Marionette'
-      }
-    }
-  });
-
-  require(['jquery', 'app'], function() {});
+  window.RegisterController = function($scope, $http) {
+    return $scope.register = function(user) {
+      return $http.post('Register', user);
+    };
+  };
 
 }).call(this);
